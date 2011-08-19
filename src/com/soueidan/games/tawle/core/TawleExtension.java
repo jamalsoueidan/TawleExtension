216f23a1499099c60b581e2e4257a21e84041f4d
Jamal Soueidan <jamal@soueidan.com>
2011-08-19 03:17:55 +0200

    Init

---
+++ src/com/soueidan/games/tawle/core/TawleExtension.java
@@ -0,0 +1,13 @@
+package com.soueidan.games.tawle.core;
+
+import com.smartfoxserver.v2.extensions.SFSExtension;
+
+public class TawleExtension extends SFSExtension {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		trace("Hello Extension");
+	}
+
+}
